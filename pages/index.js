975066f84e6da8df8d5a227627a22f21eafa37bb
B-Houssem <houssembendaas00@gmail.com>
2023-05-17 21:45:57 +0100

    updated text for serivces and portfolio

--- pages/index.js
+++ pages/index.js
@@ -79,16 +79,7 @@ export default function Home() {
           <div>
             <h3 className="text-3xl py-1 dark:text-white">Services I offer</h3>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              Since the beginning of my journey as a freelance designer and
-              developer, I've done remote work for
-              <span className="text-teal-500"> agencies </span>
-              consulted for <span className="text-teal-500">startups </span>
-              and collaborated with talanted people to create digital products
-              for both business and consumer use.
-            </p>
-            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              I offer from a wide range of services, including brand design,
-              programming and teaching.
+            Over the course of my academic and professional career, I have acquired a vast amount of expertise and skills in both front-end and back-end development, using a variety of tools and frameworks. I always aim to expand my horizons and further deepen my knowledge within the ever-evolving field of web development.
             </p>
           </div>
           <div className="lg:flex  gap-10">
@@ -165,17 +156,12 @@ export default function Home() {
           <div>
             <h3 className="text-3xl py-1 dark:text-white ">Portofolio</h3>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              Since the beginning of my journey as a freelance designer and
-              developer, I've done remote work for
-              <span className="text-teal-500"> agencies </span>
-              consulted for <span className="text-teal-500">startups </span>
-              and collaborated with talanted people to create digital products
-              for both business and consumer use.
+            Since the beginning of my journey as a freelance developer, I've done personal projects as well as remote work for
+              <span className="text-teal-500"> a startup </span>
+              called <span className="text-teal-500">Safe Digital Health </span>
+              in collaboratation with talanted people to create digital products for both business and consumer use.
             </p>
-            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              I offer from a wide range of services, including brand design,
-              programming and teaching.
-            </p>
+                
           </div>
           <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
             <div className="basis-1/3 flex-1 ">
